fcb18e8ccf3375583b15ba99fd4e458a89e94d3d
chaz6chez <250220719@qq.com>
2022-06-23 15:25:57 +0800

    update

--- src/helpers.php
+++ src/helpers.php
@@ -1,16 +1,4 @@
 <?php
 declare(strict_types=1);
 
-namespace Workbunny\WebmanNacos;
-
-use Workerman\Worker;
-
-function reload(string $file)
-{
-    Worker::log($file . ' update and reload. ');
-    if(extension_loaded('posix') and extension_loaded('pcntl')){
-        posix_kill(posix_getppid(), SIGUSR1);
-    }else{
-        Worker::reloadAllWorkers();
-    }
-}
+namespace Workbunny\WebmanRabbitMQ;
