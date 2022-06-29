4b2b7dfc0b7a711faefae6d5011da0726ccea3c1
chaz6chez <250220719@qq.com>
2022-06-30 00:45:30 +0800

    update

--- src/command/InitWorkbunnyWebmanRabbitMQBuilder.php
+++ src/command/InitWorkbunnyWebmanRabbitMQBuilder.php
@@ -83,13 +83,18 @@ use Workbunny\WebmanRabbitMQ\FastBuilder;
 
 class {$name} extends FastBuilder
 {
-    protected int \$prefetch_size = 1;
+    // QOS 大小
+    protected int \$prefetch_size = 0;
+    // QOS 数量
     protected int \$prefetch_count = 0;
+    // QOS 是否全局
     protected bool \$is_global = false;
-
-
+    // 是否延迟队列
+    protected bool \$delayed = false;
+    // 消费回调
     public function handler(BunnyMessage \$message, BunnyChannel \$channel, BunnyClient \$client): string
     {
+        // TODO 消费需要的回调逻辑
         var_dump('请重写 {$name}::handler() ');
         return Constants::ACK;
         # Constants::NACK
