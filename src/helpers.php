7b4ad2d0346037c419e40eecd83feb992c8f339f
chaz6chez <250220719@qq.com>
2022-09-05 18:07:07 +0800

    fixed #1

--- src/helpers.php
+++ src/helpers.php
@@ -18,7 +18,7 @@ function sync_publish(FastBuilder $builder, string $body, ?array $headers = null
 {
     $message = $builder->getMessage();
     if(
-        ($message->getExchangeType() !== Constants::DELAYED and $headers['x-delay'] ?? 1) or
+        ($message->getExchangeType() !== Constants::DELAYED and $headers['x-delay'] ?? 0) or
         ($message->getExchangeType() === Constants::DELAYED and !($headers['x-delay'] ?? 0))
     ){
         throw new WebmanRabbitMQException('Invalid publish. ');
@@ -42,7 +42,7 @@ function async_publish(FastBuilder $builder, string $body, ?array $headers = nul
 {
     $message = $builder->getMessage();
     if(
-        ($message->getExchangeType() !== Constants::DELAYED and $headers['x-delay'] ?? 1) or
+        ($message->getExchangeType() !== Constants::DELAYED and $headers['x-delay'] ?? 0) or
         ($message->getExchangeType() === Constants::DELAYED and !($headers['x-delay'] ?? 0))
     ){
         throw new WebmanRabbitMQException('Invalid publish. ');
